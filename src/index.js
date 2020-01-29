9634f859a927574d5f9bcd4744aa4f118f1cfc50
Jay <chunsaa@gmail.com>
2020-01-29 03:21:02 -0500

    add netlify env

--- src/index.js
+++ src/index.js
@@ -2,6 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import axios from "axios";
+
+if (process.env.REACT_APP_API_BASE_URL) {
+  axios.defaults.baseURL = process.env.REACT_APP_API_BASE_URL;
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
