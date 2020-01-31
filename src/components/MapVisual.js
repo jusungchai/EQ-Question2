deba0d90be51221638e4e6aaa0e95048469f92a2
Jay <chunsaa@gmail.com>
2020-01-31 04:18:25 -0500

    add intensity to markers and option to choose metrics

--- src/components/MapVisual.js
+++ src/components/MapVisual.js
@@ -2,17 +2,32 @@ import React, { useState, useEffect } from 'react'
 import GoogleMapReact from 'google-map-react'
 import Marker from './Marker'
 import axios from 'axios'
+import Select from 'react-select'
 
 export default function MapVisual(props) {
+
+  const options = [
+    { value: 'events', label: 'Intensity by Total Events' },
+    { value: 'impressions', label: 'Intensity by Total Impressions' },
+    { value: 'clicks', label: 'Intensity by Total Clicks' },
+    { value: 'revenue', label: 'Intensity by Total Revenue' },
+    { value: null, label: 'No Intensity' }
+  ]
+
   const [state, setState] = useState({
+    metricOption: null,
     poi: null,
     loading: true
   })
 
   useEffect(() => {
-    const pois = axios.get('/poi')
+    const pois = axios.get('/totalMetrics')
+    const statsHourly = axios.get('/stats/hourly1')
+    const eventsHourly = axios.get('/events/hourly1')
     Promise.all([
-      pois
+      pois,
+      statsHourly,
+      eventsHourly
     ]).then((all) => {
       setState({
         poi: all[0].data,
@@ -24,21 +39,96 @@ export default function MapVisual(props) {
   const generateMarkers = () => {
     console.log(state.poi)
     let key = 0;
-    return state.poi.map((elm) => {
-      key++
-      return <Marker
-        key={key}
-        lat={elm.lat}
-        lng={elm.lon}
-        name={elm.name}
-      />
-    })
+    let intensities;
+    let minIntensity;
+    let maxIntensity;
+    switch (state.metricOption) {
+      case "events":
+        intensities = state.poi.map((elm) => parseFloat(elm.events))
+        minIntensity = Math.min(...intensities) 
+        maxIntensity = Math.max(...intensities)
+        return state.poi.map((elm) => {
+          key++
+          return <Marker
+            key={key}
+            lat={elm.lat}
+            lng={elm.lon}
+            name={elm.name}
+            intensity={parseFloat(elm.events)}
+            minIntensity={minIntensity}
+            maxIntensity={maxIntensity}
+          />
+        })
+      case "impressions":
+        intensities = state.poi.map((elm) => parseFloat(elm.impressions))
+        minIntensity = Math.min(...intensities) 
+        maxIntensity = Math.max(...intensities)
+        return state.poi.map((elm) => {
+          key++
+          return <Marker
+            key={key}
+            lat={elm.lat}
+            lng={elm.lon}
+            name={elm.name}
+            intensity={parseFloat(elm.impressions)}
+            minIntensity={minIntensity}
+            maxIntensity={maxIntensity}
+          />
+        })
+      case "clicks":
+        intensities = state.poi.map((elm) => parseFloat(elm.clicks))
+        minIntensity = Math.min(...intensities) 
+        maxIntensity = Math.max(...intensities)
+        return state.poi.map((elm) => {
+          key++
+          return <Marker
+            key={key}
+            lat={elm.lat}
+            lng={elm.lon}
+            name={elm.name}
+            intensity={parseFloat(elm.clicks)}
+            minIntensity={minIntensity}
+            maxIntensity={maxIntensity}
+          />
+        })
+      case "revenue":
+        intensities = state.poi.map((elm) => parseFloat(elm.revenue))
+        minIntensity = Math.min(...intensities) 
+        maxIntensity = Math.max(...intensities)
+        return state.poi.map((elm) => {
+          key++
+          return <Marker
+            key={key}
+            lat={elm.lat}
+            lng={elm.lon}
+            name={elm.name}
+            intensity={parseFloat(elm.revenue)}
+            minIntensity={minIntensity}
+            maxIntensity={maxIntensity}
+          />
+        })
+      default:
+        return state.poi.map((elm) => {
+          key++
+          return <Marker
+            key={key}
+            lat={elm.lat}
+            lng={elm.lon}
+            name={elm.name}
+          />
+        })
+    }
   }
 
   return state.loading ? null : (
     <React.Fragment>
       <h1>Client-Side Geo Visualizations</h1>
 
+      <Select
+        onChange={(event) => setState({ ...state, metricOption: event.value })}
+        options={options}
+      />
+      <br />
       <div style={{ height: '500px', width: '100%' }}>
         <GoogleMapReact
           bootstrapURLKeys={{ key: 'AIzaSyBQ1NimXDA1tP00n1hqmk3RDjM7vINTM78' }}
--- src/components/Marker.js
+++ src/components/Marker.js
@@ -2,8 +2,36 @@ import React from 'react'
 import "./Marker.css"
 
 export default function Marker(props) {
+  const intensityMidPoint = (props.minIntensity + props.maxIntensity)/2 
+  const lowerRatio = props.intensity/props.minIntensity
+  const midRatio = props.intensity/intensityMidPoint
+  const upperRatio = props.intensity/props.maxIntensity
+  const intensityColor = () => {
+    if (upperRatio === 0){
+      return
+    }
+    if (upperRatio === 1){
+      return "#ff0000"
+    }
+    if (midRatio === 1){
+      return "#ffa200"
+    }
+    if (lowerRatio === 1){
+      return "#ffe600"
+    }
+    if (upperRatio > 0.9){
+      return "#ff4800"
+    }
+    if (upperRatio > 0.75){
+      return "#ff8000"
+    }
+    if (upperRatio < 0.5){
+      return "#f7ff66"
+    }
+  }
+  const color = props.intensity ? intensityColor() : null
   return (
-    <div className={"pin"}>
+    <div className={"pin"} style={{backgroundColor: color}}>
       {props.name}
     </div>
   )
